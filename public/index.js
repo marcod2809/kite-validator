222cd1c5c7f60cb2de39f13475abd4316ca784f7
Marco DUVILLIER <10125497@leroymerlin.fr>
2020-12-22 12:15:38 +0100

    feat: modifs

--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ fetch('/weather')
      let divWind = document.getElementById('windspeed')
      let divTemp = document.getElementById('outsidetemp')
      let imgDiv = document.getElementById('illustration')
-
     divWind.innerHTML = data.windSpeed + " knots";
     divTemp.innerHTML = data.temperature + " degrees";
     if (data.overallRideScoring === true){
